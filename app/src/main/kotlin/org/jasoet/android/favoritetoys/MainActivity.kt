adbbb0966659dd9c2629a9ddbcbc9618bf5d4c9e
Deny Prasetyo <jasoet87@gmail.com>
2016-12-13 01:21:34 +0700

    Enable Kotlin Android Extensions

--- app/src/main/kotlin/org/jasoet/android/favoritetoys/MainActivity.kt
+++ app/src/main/kotlin/org/jasoet/android/favoritetoys/MainActivity.kt
@@ -2,11 +2,14 @@ package org.jasoet.android.favoritetoys
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        text_view.text = getString(R.string.new_text)
     }
 }
