8d5692a346f98138963fcda3e1ed89d9e5e813f8
Deny Prasetyo <jasoet87@gmail.com>
2016-12-13 00:55:25 +0700

    Kotlin source for androidTest

---
+++ app/src/androidTest/kotlin/org/jasoet/android/favoritetoys/ExampleInstrumentedTest.kt
@@ -0,0 +1,26 @@
+package org.jasoet.android.favoritetoys
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.assertEquals
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+
+ * @see [Testing documentation](http://d.android.com/tools/testing)
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    @Throws(Exception::class)
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+
+        assertEquals("org.jasoet.android.favoritetoys", appContext.packageName)
+    }
+}
